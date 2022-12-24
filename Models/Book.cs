a2e607bfb76a8dadcc3ad914a970c4529a3aedc2
phucnhgch18652 <65522550+phucnhgch18652@users.noreply.github.com>
2022-12-24 17:50:15 +0700

    3

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,17 +6,10 @@ namespace TheNew1.Models
     {
         [Key]
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Please enter name of the Book")]
-        [Display(Name = "Name")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Please enter Category of the book")]
         public string Category { get; set; }
-        [Required(ErrorMessage = "Please enter price of the book")]
         public double Price { get; set; }
-        [Required(ErrorMessage = "Please enter Author of the book")]
         public string Author { get; set; }
-        [Required(ErrorMessage = "Please enter picture of the book")]
         public string Picture { get; set; }
     }
 }
